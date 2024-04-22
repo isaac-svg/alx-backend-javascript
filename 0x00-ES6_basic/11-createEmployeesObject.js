f1c8cf04ab4b1fe2bb378b7a5d042466ba7cffdf
isaac-svg <isaacsakyi009@gmail.com>
2024-04-22 19:14:27 +0000

    RENAME files

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,3 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const temp = {};
-  temp[departmentName] = employees;
-  return temp;
+  return { [departmentName]: employees };
 }

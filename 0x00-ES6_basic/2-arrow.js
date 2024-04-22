a2df2e7b661f0dca7a6c6eb49497c925f1a0592b
isaac-svg <isaacsakyi009@gmail.com>
2024-04-22 19:55:55 +0000

    FIX BUG

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,12 +1,8 @@
-class getNeighborhoodsList {
-  constructor() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  }
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  addNeighborhood(newNeighborhood) {
+  this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
-  }
+  };
 }
-
-export default getNeighborhoodsList;
